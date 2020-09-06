f9d108bafb27c97a296669c6468618e4e5efc628
vicanso <vicansocanbico@gmail.com>
2020-09-06 14:05:55 +0800

    refactor: enhance viperx

--- viperx.go
+++ viperx.go
@@ -23,8 +23,7 @@ import (
 )
 
 type ViperX struct {
-	// ins viper instance
-	ins *viper.Viper
+	*viper.Viper
 	// ConfigType config type
 	ConfigType string
 }
@@ -32,10 +31,10 @@ type ViperX struct {
 // New new viperx
 func New(configType string) *ViperX {
 	viperX := &ViperX{
-		ins:        viper.New(),
 		ConfigType: configType,
 	}
-	viperX.ins.SetConfigType(configType)
+	viperX.Viper = viper.New()
+	viperX.SetConfigType(viperX.ConfigType)
 	return viperX
 }
 
@@ -49,236 +48,156 @@ func (vx *ViperX) ReadConfig(readers ...io.Reader) error {
 			return err
 		}
 		for k, v := range v.AllSettings() {
-			vx.ins.SetDefault(k, v)
+			vx.SetDefault(k, v)
 		}
 	}
 	return nil
 }
 
-// GetBool get value(bool) from config
-func (vx *ViperX) GetBool(key string) bool {
-	return vx.ins.GetBool(key)
-}
-
-// GetInt get value(int) from config
-func (vx *ViperX) GetInt(key string) int {
-	return vx.ins.GetInt(key)
-}
-
 // GetIntDefault get value(int) from config,
 // if value is 0, return the default value
 func (vx *ViperX) GetIntDefault(key string, defaultValue int) int {
-	v := vx.ins.GetInt(key)
+	v := vx.GetInt(key)
 	if v == 0 {
 		return defaultValue
 	}
 	return v
-}
-
-// GetInt32 get value(int32) from config
-func (vx *ViperX) GetInt32(key string) int32 {
-	return vx.ins.GetInt32(key)
 }
 
 // GetInt32Default get value(int32) from config,
 // if value is 0, return the default value
 func (vx *ViperX) GetInt32Default(key string, defaultValue int32) int32 {
-	v := vx.ins.GetInt32(key)
+	v := vx.GetInt32(key)
 	if v == 0 {
 		return defaultValue
 	}
 	return v
-}
-
-// GetInt64 get value(int64) from config
-func (vx *ViperX) GetInt64(key string) int64 {
-	return vx.ins.GetInt64(key)
 }
 
 // GetInt64Default get value(int64) from config,
 // if value is 0, return the default value
 func (vx *ViperX) GetInt64Default(key string, defaultValue int64) int64 {
-	v := vx.ins.GetInt64(key)
+	v := vx.GetInt64(key)
 	if v == 0 {
 		return defaultValue
 	}
 	return v
-}
-
-// GetUint get value(uint) from config
-func (vx *ViperX) GetUint(key string) uint {
-	return vx.ins.GetUint(key)
 }
 
 // GetUintDefault get value(uint) from config,
 // if value is 0, return the default value
 func (vx *ViperX) GetUintDefault(key string, defaultValue uint) uint {
-	v := vx.ins.GetUint(key)
+	v := vx.GetUint(key)
 	if v == 0 {
 		return defaultValue
 	}
 	return v
-}
-
-// GetUint32 get value(uint32) from config
-func (vx *ViperX) GetUint32(key string) uint32 {
-	return vx.ins.GetUint32(key)
 }
 
 // GetUint32Default get value(uint32) from config,
 // if value is 0, return the default value
 func (vx *ViperX) GetUint32Default(key string, defaultValue uint32) uint32 {
-	v := vx.ins.GetUint32(key)
+	v := vx.GetUint32(key)
 	if v == 0 {
 		return defaultValue
 	}
 	return v
-}
-
-// GetUint64 get value(uint64) from config
-func (vx *ViperX) GetUint64(key string) uint64 {
-	return vx.ins.GetUint64(key)
 }
 
 // GetUint64Default get value(uint64) from config,
 // if value is 0, return the default value
 func (vx *ViperX) GetUint64Default(key string, defaultValue uint64) uint64 {
-	v := vx.ins.GetUint64(key)
+	v := vx.GetUint64(key)
 	if v == 0 {
 		return defaultValue
 	}
 	return v
-}
-
-// GetFloat64 get value(float64) from config
-func (vx *ViperX) GetFloat64(key string) float64 {
-	return vx.ins.GetFloat64(key)
 }
 
 // GetFloat64Default get value(float64) from config,
 // if value is 0, return the default value
 func (vx *ViperX) GetFloat64Default(key string, defaultValue float64) float64 {
-	v := vx.ins.GetFloat64(key)
+	v := vx.GetFloat64(key)
 	if v == 0 {
 		return defaultValue
 	}
 	return v
 }
 
-// GetString get value(string) from config
-func (vx *ViperX) GetString(key string) string {
-	return vx.ins.GetString(key)
-}
-
 // GetStringDefault get value(string) from config,
 // if value is 0, return the default value
 func (vx *ViperX) GetStringDefault(key, defaultValue string) string {
-	v := vx.ins.GetString(key)
+	v := vx.GetString(key)
 	if len(v) == 0 {
 		return defaultValue
 	}
 	return v
 }
 
-// GetTime get value(time.Time) from config
-func (vx *ViperX) GetTime(key string) time.Time {
-	return vx.ins.GetTime(key)
-}
-
 // GetTimeDefault get value(time.Time) from config,
 // if value is 0, return the default value
 func (vx *ViperX) GetTimeDefault(key string, defaultValue time.Time) time.Time {
-	v := vx.ins.GetTime(key)
+	v := vx.GetTime(key)
 	if v.IsZero() {
 		return defaultValue
 	}
 	return v
 }
 
-// GetDuration get value(time.Duration) from config
-func (vx *ViperX) GetDuration(key string) time.Duration {
-	return vx.ins.GetDuration(key)
-}
-
 // GetDurationDefault get value(time.Duration) from config,
 // if value is 0, return the default value
 func (vx *ViperX) GetDurationDefault(key string, defaultValue time.Duration) time.Duration {
-	v := vx.ins.GetDuration(key)
+	v := vx.GetDuration(key)
 	if v == 0 {
 		return defaultValue
 	}
 	return v
 }
 
-// GetIntSlice get value([]int) from config
-func (vx *ViperX) GetIntSlice(key string) []int {
-	return vx.ins.GetIntSlice(key)
-}
-
 // GetIntSliceDefault get value([]int) from config,
 // it len(value) is 0, return the default value
 func (vx *ViperX) GetIntSliceDefault(key string, defaultValue []int) []int {
-	v := vx.ins.GetIntSlice(key)
+	v := vx.GetIntSlice(key)
 	if len(v) == 0 {
 		return defaultValue
 	}
 	return v
-}
-
-// GetStringSlice get value([]string) from config
-func (vx *ViperX) GetStringSlice(key string) []string {
-	return vx.ins.GetStringSlice(key)
 }
 
 // GetStringSliceDefault get value([]string) from config,
 // if len(value) is 0, return the default value
 func (vx *ViperX) GetStringSliceDefault(key string, defaultValue []string) []string {
-	v := vx.ins.GetStringSlice(key)
+	v := vx.GetStringSlice(key)
 	if len(v) == 0 {
 		return defaultValue
 	}
 	return v
-}
-
-// GetStringMap get value(map[string]interface{}) from config
-func (vx *ViperX) GetStringMap(key string) map[string]interface{} {
-	return vx.ins.GetStringMap(key)
 }
 
 // GetStringMapDefault get value(map[string]interface{}) from config,
 // if len(value) is 0, return the default value
 func (vx *ViperX) GetStringMapDefault(key string, defaultValue map[string]interface{}) map[string]interface{} {
-	v := vx.ins.GetStringMap(key)
+	v := vx.GetStringMap(key)
 	if len(v) == 0 {
 		return defaultValue
 	}
 	return v
-}
-
-// GetStringMapString get value(map[string]string) from config
-func (vx *ViperX) GetStringMapString(key string) map[string]string {
-	return vx.ins.GetStringMapString(key)
 }
 
 // GetStringMapStringDefault get value(map[string]string) from config,
 // if len(value) is 0, return the default value
 func (vx *ViperX) GetStringMapStringDefault(key string, defaultValue map[string]string) map[string]string {
-	v := vx.ins.GetStringMapString(key)
+	v := vx.GetStringMapString(key)
 	if len(v) == 0 {
 		return defaultValue
 	}
 	return v
 }
 
-// GetStringMapStringSlice get value(map[string][]string) from config
-func (vx *ViperX) GetStringMapStringSlice(key string) map[string][]string {
-	return vx.ins.GetStringMapStringSlice(key)
-}
-
 // GetStringMapStringSliceDefault get value(map[string][]string) from config,
 // if len(value) is 0, return the default value
 func (vx *ViperX) GetStringMapStringSliceDefault(key string, defaultValue map[string][]string) map[string][]string {
-	v := vx.ins.GetStringMapStringSlice(key)
+	v := vx.GetStringMapStringSlice(key)
 	if len(v) == 0 {
 		return defaultValue
 	}
@@ -289,7 +208,7 @@ func (vx *ViperX) GetStringMapStringSliceDefault(key string, defaultValue map[st
 // then use the value as key, get result from env,
 // if len(value) is 0, then return the config's value, otherwise return the env's value
 func (vx *ViperX) GetStringFromENV(key string) string {
-	v := vx.ins.GetString(key)
+	v := vx.GetString(key)
 	value := os.Getenv(v)
 	if len(value) != 0 {
 		return value
